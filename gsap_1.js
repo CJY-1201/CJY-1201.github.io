24d34a67c80a72d7b68c086a141b89b4ecf8aebf
CJY-1201 <ccj59834@gmail.com>
2024-10-31 11:23:53 +0900

    Update gsap_1.js

--- gsap_1.js
+++ gsap_1.js
@@ -1,6 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(Flip);
-
+const element = document.querySelector('.my-element');
 gsap.flip(element, {
     duration: 0.5,
     ease: "power1.inOut"
