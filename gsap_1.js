abec3313174d9dfc1ff8c1743eed0011cf601c26
CJY-1201 <ccj59834@gmail.com>
2024-11-05 10:58:04 +0900

    1

--- gsap_1.js
+++ gsap_1.js
@@ -9,7 +9,7 @@ gsap.from(section, {
     scrollTrigger: {
         trigger: section,
         start: "top center", // 섹션의 상단이 뷰포트의 중앙에 도달할 때 애니메이션 시작
-        toggleActions: "play none none reverse", // 스크롤 시 애니메이션 재생 및 역재생
+        toggleActions: "play reverse", // 스크롤 시 애니메이션 재생 및 역재생
     },
     opacity: 0, // 초기 투명도
     y: 50, // 아래에서 위로 이동
