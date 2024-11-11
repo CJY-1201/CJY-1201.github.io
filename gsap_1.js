42c880f302a2842488780ce0bb38b866001748eb
CJY-1201 <ccj59834@gmail.com>
2024-11-11 12:51:03 +0900

    q

--- gsap_1.js
+++ gsap_1.js
@@ -1,6 +1,7 @@
 // gsap_1.js
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(ScrollToPlugin);
+gsap.registerPlugin(ScrambleTextPlugin);
 
 window.addEventListener('wheel', function(event) {
     const logo = document.getElementById('logo');
@@ -10,3 +11,29 @@ window.addEventListener('wheel', function(event) {
         logo.style.transform = 'scale(1.5)'; // 스크롤 업 시 원래 크기로
     }
 });
+
+const lenis = new Lenis({
+    duration: 0.1,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+    });
+    
+    lenis.on('scroll', (e) => {
+    console.log(e)
+    })
+    
+    function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+    }
+    
+    requestAnimationFrame(raf);
+
+gsap.to("scramble", {
+        scrambleText: {
+            text: "Project.Lily",
+            chars: "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()",
+            speed: 0.25,
+            revealDelay: 0.5
+        },
+        duration: 3
+    });
